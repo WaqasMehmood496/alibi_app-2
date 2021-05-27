cf3cca40713b19d72d6b81b87a9c0f23578cfb4e
apple <apple@apples-macbook-2.local>
2021-05-27 15:47:31 +0500

    Initial Commit

---
+++ alibi_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alibi_app
+//
+//  Created by Uzair Masood on 5/27/21.
+//  Copyright © 2021 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
